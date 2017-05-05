0138d56c74e43099f8aa1ec799f02cce2d13c9bb
Tyler Fink <tfink419@gmail.com>
2017-05-04 23:56:12 -0600

    Google Auth connect and login

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #object that represents the application
 from flask import Flask, render_template, request, redirect, url_for, jsonify, Response
-from flask_bootstrap import Bootstrap 
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required, utils as security_utils
 import flask_mail as mail
+from flask_social import Social
+from flask_social.datastore import SQLAlchemyConnectionDatastore
 
 from os import environ as ENV
 
@@ -21,6 +23,10 @@ app.config['DEBUG'] = True
 app.config['SECURITY_PASSWORD_HASH'] = 'sha512_crypt'
 app.config['SECURITY_PASSWORD_SALT'] = ENV['SECURITY_PASSWORD_SALT']
 app.config['SECRET_KEY'] = ENV['SECRET_KEY']
+app.config['SOCIAL_GOOGLE'] = {
+    'consumer_key': ENV['GOOGLE_LOGIN_CLIENT_ID'],
+    'consumer_secret': ENV['GOOGLE_LOGIN_CLIENT_SECRET']
+}
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
 # app.config['MAIL_SERVER'] = 'smtp.example.com'
 # app.config['MAIL_PORT'] = 465
@@ -49,9 +55,34 @@ class User(db.Model, UserMixin):
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
+    connections = db.relationship('Connection',
+            backref=db.backref('user', lazy='joined'), cascade="all")
+
+class Connection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    provider_id = db.Column(db.String(255))
+    provider_user_id = db.Column(db.String(255))
+    access_token = db.Column(db.String(255))
+    secret = db.Column(db.String(255))
+    display_name = db.Column(db.PickleType)
+    full_name = db.Column(db.PickleType)
+    profile_url = db.Column(db.String(512))
+    image_url = db.Column(db.String(512))
+    # rank = db.Column(db.Integer)
+    # full_name = db.relationship('FullName',
+    #         backref=db.backref('connection', lazy='joined'), cascade="all")
+    
+class FullName(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    connection_id = db.Column(db.Integer, db.ForeignKey('connection.id'))
+    givenName = db.Column(db.String(255))
+    familyName = db.Column(db.String(255))
 
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
+social_datastore = SQLAlchemyConnectionDatastore(db, Connection)
+social = Social(app, social_datastore)
 
 db_user = ENV['DB_USER']
 db_pw = ENV['DB_PW']
@@ -83,7 +114,6 @@ logging.debug("after event_tables")
 @app.route("/protected/",methods=["GET"])
 @login_required
 def protected():
-    
 	return Response(response="Hello Protected World!", status=200)
 
 #decorators 
@@ -103,19 +133,29 @@ def remove_item(name):
 		event_tables.remove(name)
 	return redirect(url_for('index'))
 
+@app.route('/connect')
+@login_required
+def connect():
+    return render_template(
+        'connect.html',
+        content='Connect Account Page',
+        conn=social.google.get_connection())
+
 #API interactions 
 @app.route('/api/items')
 def get_items():
 	#jsonify, take a python based structure and convert to JSON format to be consumed by a machine
 	global event_tables
 	return jsonify({'items': event_tables})
-    
+
 #User for test
 @app.before_first_request
 def create_user():
     db.create_all()
-    user_datastore.create_user(email='tyler_fink', password='Pass1234')
-    user_datastore.create_user(email='sanj_chakra', password='Pass1234')
+    user_datastore.create_user(email=ENV['TEST_USER1'], password=ENV['TEST_PW1'])
+    user_datastore.create_user(email=ENV['TEST_USER2'], password=ENV['TEST_PW2'])
+    user_datastore.create_user(email=ENV['GTEST_USER1'], password=ENV['GTEST_PW1'])
+    user_datastore.create_user(email=ENV['GTEST_USER2'], password=ENV['GTEST_PW2'])
     db.session.commit()
 
 if __name__ == '__main__':
